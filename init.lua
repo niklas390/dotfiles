6110f39aaad502d7bdbe35b5559063944aa656da
niklas390 <niklas.bender.business@gmail.com>
2025-09-05 15:37:24 +0200

    Update init.lua

--- init.lua
+++ init.lua
@@ -62,16 +62,24 @@ require('mini.pick').setup()
 
 map('n', '<leader>f', ":Pick files<CR>")
 
+
 -- Lookup this for language server names:
 -- https://github.com/neovim/nvim-lspconfig/blob/master/doc/configs.md
-require("lspconfig")["gopls"].setup({
-	on_attach = function(client, bufnr)
-      vim.lsp.completion.enable(true, client.id, bufnr, {
-		autotrigger = true,
-		convert = function(item)
-          return { abbr = item.label:gsub("%b()", "") }
-		end,
-      })
-      vim.keymap.set("i", "<C-space>", vim.lsp.completion.get, { desc = "trigger autocompletion" })
-    end
-})
+servers = {"gopls", "rust_analyzer", "clangd"}
+
+vim.lsp.enable(servers)
+
+for _, language_server in pairs(servers)
+do
+	require("lspconfig")[language_server].setup({
+		on_attach = function(client, bufnr)
+				vim.lsp.completion.enable(true, client.id, bufnr, {
+			autotrigger = true,
+			convert = function(item)
+						return { abbr = item.label:gsub("%b()", "") }
+			end,
+				})
+				vim.keymap.set("i", "<C-space>", vim.lsp.completion.get, { desc = "trigger autocompletion" })
+			end
+	})
+end
